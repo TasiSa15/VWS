29dec6a07230647a21e341cddd97061e6c37b50d
TasiSa15 <33129363+TasiSa15@users.noreply.github.com>
2018-12-29 14:29:26 +0100

    JUnit-Tests

---
+++ src/testing/ConnectFourTest.java
@@ -0,0 +1,16 @@
+package testing;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConnectFourTest {
+
+	@Test
+	public void test() {
+		assertTrue("Not yet implemented", true);
+	}
+
+	
+}
+
+
